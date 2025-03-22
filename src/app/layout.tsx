c2aaf72ff0b35589bfb3f07f087a5f15954605d3
Artur <artur.lihitskiy@gmail.com>
2025-03-22 18:38:05 +0200

    initialize project structure with essential components and configurations

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+
+import '../shared/styles/globals.css'
+
+const geistSans = Geist({
+	variable: '--font-geist-sans',
+	subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+	variable: '--font-geist-mono',
+	subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+	title: {
+		absolute: 'Authorization Project',
+		template: '%s | Authorization Project'
+	},
+	description: 'This is a fullstack project for authorization.'
+}
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang='en'>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	)
+}
